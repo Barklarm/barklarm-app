bd9554a08d17168752477a58a4915077625549a3
kanekotic <me@kanekotic.com>
2022-05-05 09:26:12 +0200

    save and delete new observables

--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,6 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@mui/material/Button';
+import SpeedDialIcon from '@mui/material/SpeedDialIcon';
+import Stack from '@mui/material/Stack';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
+import TextField from '@mui/material/TextField';
+import { IndeterminateCheckBox } from '@mui/icons-material';
+
 
 export const App = () => {
-    window.electron.store.set("unicorn", "🦄");
-    return (<>I'm a {window.electron.store.get("unicorn")}</>);
+    const [observables, setObservables] = useState(window.electron.store.get("observables") || []);
+    const deleteByIndex = (index: number) => {
+        setObservables(observables.filter((_: any, filterIndex:number) => {
+            
+            console.log("index to delete", index, "current index", filterIndex)
+            return filterIndex != index
+        }))
+    }
+    return <>
+        <Stack 
+            spacing={2}
+        >
+            {observables.map((observable: any, index: number) => 
+                <Accordion>
+                        <AccordionSummary
+                        expandIcon={<ExpandMoreIcon />}
+                        aria-controls="panel1a-content"
+                        id="panel1a-header"
+                        >
+                            <Typography>Observable {observable.name}</Typography>
+                        </AccordionSummary>
+                        <AccordionDetails>
+                            <Stack spacing={2}>
+                                <TextField id="outlined-basic" label="Name" variant="outlined" value={observable.name} />
+                                <TextField id="outlined-basic" label="Key" variant="outlined" value={observable.key} />
+                                <TextField id="outlined-basic" label="URL" variant="outlined" value={observable.url}/>
+                                <Stack spacing={2} direction="row" justifyContent="flex-end">
+                                    <Button onClick={() => deleteByIndex(index)}> Delete </Button>
+                                </Stack>
+                            </Stack> 
+                        </AccordionDetails>
+                </Accordion>)}
+            
+            <Stack spacing={2} direction="row" justifyContent="flex-end">
+                <Button 
+                    variant="contained" 
+                    onClick={() => window.electron.store.set("observables", observables)}
+                > 
+                    Save 
+                </Button>
+                <Button
+                    variant="contained"
+                    onClick={() =>setObservables([...observables, {key: "", url: "", name: ""}])}
+                >
+                    <SpeedDialIcon />
+                </Button>
+            </Stack>
+            
+        </Stack>
+      
+    </>
 }
