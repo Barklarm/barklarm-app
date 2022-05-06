fffc313791c5d901c92ad4280689c83e64d2d21d
kanekotic <me@kanekotic.com>
2022-05-06 16:29:53 +0200

    added notifications

---
+++ src/main/NotificationManager.ts
@@ -0,0 +1,20 @@
+import { Notification, nativeImage } from 'electron';
+import { join } from 'path';
+import { State } from "./observers/ObserverManager";
+
+export class NotificationManager {
+    private readonly okIconPath: string = join(__dirname, '..','assets', 'ok_icon.png');
+    private readonly FailIconPath: string = join(__dirname, '..','assets', 'fail_icon.png');
+
+    public updateNotifications(old: State[], actual: State[]){
+        actual.forEach(current => {
+            const previous = old.find(oldState => oldState.name === current.name) || { isSuccess: true } as any
+            if (!current.isSuccess && previous.isSuccess){
+               return new Notification({ title: "Fail", body: `${current.name} failed`, icon: nativeImage.createFromPath(this.FailIconPath)}).show()
+            }
+            if (current.isSuccess && !previous.isSuccess){
+               return new Notification({ title: "Fix", body: `${current.name} fixed`, icon: nativeImage.createFromPath(this.okIconPath)}).show()
+            }
+        })
+    }
+}
