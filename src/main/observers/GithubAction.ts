1b0114e75a12c742e56d7d2fe8c9d27b328465a6
Dimitar Apostolov <>
2022-06-09 22:02:57 +0200

    feat: add alias for each flow

--- src/main/observers/GithubAction.ts
+++ src/main/observers/GithubAction.ts
@@ -1,16 +1,13 @@
 import { Octokit } from "octokit";
 import { Observer, State, ObserverConfiguration } from "./ObserverManager";
 
-
-
 export type GithubActionConfiguration = ObserverConfiguration & {
   authToken: string;
   owner: string;
   repo: string;
   workflowId: string;
-}
-
-
+  alias?: string;
+};
 
 export class GithubAction implements Observer {
   private octokit: Octokit;
@@ -18,50 +15,67 @@ export class GithubAction implements Observer {
   private repo: string;
   private workflowId: string;
 
-  constructor({authToken, owner, repo, workflowId}: GithubActionConfiguration){
-      this.octokit = new Octokit({
-        auth: authToken
-      });
-      this.owner=owner;
-      this.repo=repo;
-      this.workflowId=workflowId;
+  private alias: string;
+
+  constructor({
+    authToken,
+    owner,
+    repo,
+    workflowId,
+    alias,
+  }: GithubActionConfiguration) {
+    this.octokit = new Octokit({
+      auth: authToken,
+    });
+    this.owner = owner;
+    this.repo = repo;
+    this.workflowId = workflowId;
+    this.alias = alias;
   }
 
   public async getState(): Promise<State> {
-    const name = `Github: ${this.owner}/${this.repo}/${this.workflowId}`
+    const name = this.resolveAlias();
     try {
-        const response = await this.octokit.request('GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs', {
+      const response = await this.octokit.request(
+        "GET /repos/{owner}/{repo}/actions/workflows/{workflow_id}/runs",
+        {
           owner: this.owner,
           repo: this.repo,
-          workflow_id: this.workflowId
-        })
-      if(response.status != 200 && response.data.total_count > 0)
+          workflow_id: this.workflowId,
+        }
+      );
+      if (response.status != 200 && response.data.total_count > 0)
         return {
           name,
           isReachable: false,
-        }
-      const {conclusion} = response.data.workflow_runs[0]
+        };
+      const { conclusion } = response.data.workflow_runs[0];
       if (conclusion == null) {
         return {
           name,
           isReachable: true,
-          isRunning: true
-        }
+          isRunning: true,
+        };
       }
       return {
         name,
         isReachable: true,
         isRunning: false,
-        isSuccess: conclusion === "success"
-      }
-    
+        isSuccess: conclusion === "success",
+      };
     } catch (error) {
       console.error(error);
       return {
         name,
         isReachable: false,
-      }
+      };
     }
   }
 
+  private resolveAlias() {
+    return (
+      this.alias ?? `Github: ${this.owner}/${this.repo}/${this.workflowId}`
+    );
+  }
 }
+
--- src/renderer/app.tsx
+++ src/renderer/app.tsx
@@ -1,93 +1,142 @@
-import React, { useState } from 'react';
-import Button from '@mui/material/Button';
-import Stack from '@mui/material/Stack';
-import Accordion from '@mui/material/Accordion';
-import AccordionSummary from '@mui/material/AccordionSummary';
-import AccordionDetails from '@mui/material/AccordionDetails';
-import Typography from '@mui/material/Typography';
-import ExpandMoreIcon from '@mui/icons-material/ExpandMore';
-import TextField from '@mui/material/TextField';
-
+import React, { useState } from "react";
+import Button from "@mui/material/Button";
+import Stack from "@mui/material/Stack";
+import Accordion from "@mui/material/Accordion";
+import AccordionSummary from "@mui/material/AccordionSummary";
+import AccordionDetails from "@mui/material/AccordionDetails";
+import Typography from "@mui/material/Typography";
+import ExpandMoreIcon from "@mui/icons-material/ExpandMore";
+import TextField from "@mui/material/TextField";
 
 export const App = () => {
-    const [observables, setObservables] = useState(window.electron.store.get("observables") || []);
-    const deleteByIndex = (index: number) => {
-        setObservables(observables.filter((_: any, currentIndex:number) => currentIndex != index))
-    }
-    const updateFieldWithValue  = (fieldName:string, index: number, value: any) => {
-        setObservables(observables.map((observable: any, currentIndex:number) => currentIndex != index
-            ? observable
-            : {...observable, [fieldName]: value}))
-    }
-    return <>
-        <Stack 
-            spacing={2}
-        >
-            {observables.map((observable: any, index: number) => 
-                <Accordion>
-                        <AccordionSummary
-                        expandIcon={<ExpandMoreIcon />}
-                        aria-controls="panel1a-content"
-                        id="panel1a-header"
-                        >
-                            <Typography>#{index+1}:{observable.owner}/{observable.repo}/{observable.workflowId}</Typography>
-                        </AccordionSummary>
-                        <AccordionDetails>
-                            <Stack spacing={2}>
-                                <TextField 
-                                    id="outlined-basic" 
-                                    label="owner" 
-                                    variant="outlined" 
-                                    value={observable.owner} 
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateFieldWithValue("owner", index, event.target.value)} 
-                                />
-                                <TextField 
-                                    id="outlined-basic" 
-                                    label="repo" 
-                                    variant="outlined" 
-                                    value={observable.repo} 
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateFieldWithValue("repo", index, event.target.value)} 
-                                />
-                                <TextField 
-                                    id="outlined-basic" 
-                                    label="Workflow Name" 
-                                    variant="outlined" 
-                                    value={observable.workflowId} 
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateFieldWithValue("workflowId", index, event.target.value)} 
-                                />
-                                <TextField 
-                                    id="outlined-basic" 
-                                    label="authorization Token" 
-                                    variant="outlined" 
-                                    type="password"
-                                    value={observable.authToken} 
-                                    onChange={(event: React.ChangeEvent<HTMLInputElement>) => updateFieldWithValue("authToken", index, event.target.value)} 
-                                />
-                                <Stack spacing={2} direction="row" justifyContent="flex-end">
-                                    <Button onClick={() => deleteByIndex(index)}> Delete </Button>
-                                </Stack>
-                            </Stack> 
-                        </AccordionDetails>
-                </Accordion>)}
-            
-            <Stack spacing={2} direction="row" justifyContent="flex-end">
-                <Button 
-                    variant="contained" 
-                    onClick={() => {
-                        window.electron.store.set("observables", observables)
-                        window.electron.app.refreshObservers()
-                    }}> 
-                    Save 
-                </Button>
-                <Button
-                    variant="contained"
-                    onClick={() =>setObservables([...observables, {type: "githubAction", authToken: "", owner: "", repo: "", workflowId: ""}])}
-                >
-                    Add
-                </Button>
-            </Stack>
-            
+  const [observables, setObservables] = useState(
+    window.electron.store.get("observables") || []
+  );
+  const deleteByIndex = (index: number) => {
+    setObservables(
+      observables.filter(
+        (_: any, currentIndex: number) => currentIndex != index
+      )
+    );
+  };
+  const updateFieldWithValue = (
+    fieldName: string,
+    index: number,
+    value: any
+  ) => {
+    setObservables(
+      observables.map((observable: any, currentIndex: number) =>
+        currentIndex != index
+          ? observable
+          : { ...observable, [fieldName]: value }
+      )
+    );
+  };
+  return (
+    <>
+      <Stack spacing={2}>
+        {observables.map((observable: any, index: number) => (
+          <Accordion>
+            <AccordionSummary
+              expandIcon={<ExpandMoreIcon />}
+              aria-controls="panel1a-content"
+              id="panel1a-header"
+            >
+              <Typography>
+                #{index + 1}:{observable.owner}/{observable.repo}/
+                {observable.workflowId}
+              </Typography>
+            </AccordionSummary>
+            <AccordionDetails>
+              <Stack spacing={2}>
+                <TextField
+                  id="outlined-basic"
+                  label="owner"
+                  variant="outlined"
+                  value={observable.owner}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    updateFieldWithValue("owner", index, event.target.value)
+                  }
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="repo"
+                  variant="outlined"
+                  value={observable.repo}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    updateFieldWithValue("repo", index, event.target.value)
+                  }
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="Workflow Name"
+                  variant="outlined"
+                  value={observable.workflowId}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    updateFieldWithValue(
+                      "workflowId",
+                      index,
+                      event.target.value
+                    )
+                  }
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="authorization Token"
+                  variant="outlined"
+                  type="password"
+                  value={observable.authToken}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    updateFieldWithValue("authToken", index, event.target.value)
+                  }
+                />
+                <TextField
+                  id="outlined-basic"
+                  label="alias"
+                  variant="outlined"
+                  value={observable.alias}
+                  onChange={(event: React.ChangeEvent<HTMLInputElement>) =>
+                    updateFieldWithValue("alias", index, event.target.value)
+                  }
+                />
+                <Stack spacing={2} direction="row" justifyContent="flex-end">
+                  <Button onClick={() => deleteByIndex(index)}> Delete </Button>
+                </Stack>
+              </Stack>
+            </AccordionDetails>
+          </Accordion>
+        ))}
+
+        <Stack spacing={2} direction="row" justifyContent="flex-end">
+          <Button
+            variant="contained"
+            onClick={() => {
+              window.electron.store.set("observables", observables);
+              window.electron.app.refreshObservers();
+            }}
+          >
+            Save
+          </Button>
+          <Button
+            variant="contained"
+            onClick={() =>
+              setObservables([
+                ...observables,
+                {
+                  type: "githubAction",
+                  authToken: "",
+                  owner: "",
+                  repo: "",
+                  workflowId: "",
+                },
+              ])
+            }
+          >
+            Add
+          </Button>
         </Stack>
-      
+      </Stack>
     </>
-}
+  );
+};
+
