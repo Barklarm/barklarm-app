0852aa78168fe952985e1a7c8681a6efbdd81eb9
Dimitar Apostolov <>
2022-06-09 22:15:16 +0200

    refactor: use ES imports consistently

--- src/index.ts
+++ src/index.ts
@@ -1,48 +1,55 @@
-import { app, ipcMain } from 'electron';
-import { appManager } from './main/AppManager';
-import { TrayMenu } from './main/TrayMenu';
-import { AppWindow } from './main/AppWindow';
-import { ObserverManager } from './main/observers/ObserverManager';
+import { app, ipcMain } from "electron";
+import { appManager } from "./main/AppManager";
+import { TrayMenu } from "./main/TrayMenu";
+import { AppWindow } from "./main/AppWindow";
+import { ObserverManager } from "./main/observers/ObserverManager";
 import "./store";
-import { NotificationManager } from './main/NotificationManager';
-import { dirname, resolve, basename } from 'path';
+import { NotificationManager } from "./main/NotificationManager";
+import { dirname, resolve, basename } from "path";
+import updateElectronApp from "update-electron-app";
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-ignore
+import hasSquirrelStartupEvents from "electron-squirrel-startup";
 
 declare const MAIN_WINDOW_WEBPACK_ENTRY: string;
 declare const MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY: string;
 
-require('update-electron-app')()
-if (require('electron-squirrel-startup')) {
-  // eslint-disable-line global-require
+updateElectronApp();
+
+if (hasSquirrelStartupEvents) {
   app.quit();
 }
 
-let updateExePath; 
-const appFolder = dirname(process.execPath)
-const exeName = basename(process.execPath)
-if (process.platform === 'win32')
-{
-    app.setAppUserModelId(app.name)
-    const updateExePath = resolve(appFolder, '..', 'Update.exe')
+let updateExePath;
+const appFolder = dirname(process.execPath);
+const exeName = basename(process.execPath);
+if (process.platform === "win32") {
+  app.setAppUserModelId(app.name);
+  const updateExePath = resolve(appFolder, "..", "Update.exe");
 }
 
 app.setLoginItemSettings({
   openAtLogin: true,
   path: updateExePath,
   args: [
-    '--processStart', `"${exeName}"`,
-    '--process-start-args', `"--hidden"`
-  ]
-})
-
-app.on('ready', () => {
-  const tray = new TrayMenu()
-  const notification = new NotificationManager()
-  const observerManager = new ObserverManager(tray, notification)
-  observerManager.refershObservers()
-  appManager.setTray(tray);
-  appManager.setWindow('AppWindow', new AppWindow(MAIN_WINDOW_WEBPACK_ENTRY, MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY));
-  ipcMain.on('electron-refresh-observers', () => {
-    observerManager.refershObservers()
-  });
+    "--processStart",
+    `"${exeName}"`,
+    "--process-start-args",
+    `"--hidden"`,
+  ],
 });
 
+app.on("ready", () => {
+  const tray = new TrayMenu();
+  const notification = new NotificationManager();
+  const observerManager = new ObserverManager(tray, notification);
+  observerManager.refershObservers();
+  appManager.setTray(tray);
+  appManager.setWindow(
+    "AppWindow",
+    new AppWindow(MAIN_WINDOW_WEBPACK_ENTRY, MAIN_WINDOW_PRELOAD_WEBPACK_ENTRY)
+  );
+  ipcMain.on("electron-refresh-observers", () => {
+    observerManager.refershObservers();
+  });
+});
