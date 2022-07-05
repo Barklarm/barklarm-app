c21025292d2c3d1b4a886640686f442d9f2f9273
kanekotic <me@kanekotic.com>
2022-07-05 05:06:00 +0200

    Alvaro | notifications will show on all transitions

--- src/main/NotificationManager.ts
+++ src/main/NotificationManager.ts
@@ -1,29 +1,38 @@
-import { Notification, nativeImage } from 'electron';
+import { Notification, nativeImage, NotificationConstructorOptions } from 'electron';
 import { join } from 'path';
 import { Status } from '../types/Status';
 import { State } from '../types/State';
+import { MapType } from '../types/MapType';
 
 export class NotificationManager {
-  private readonly okIconPath: string = join(__dirname, '..', 'assets', 'ok_icon_big.png');
-  private readonly FailIconPath: string = join(__dirname, '..', 'assets', 'fail_icon_big.png');
+  private readonly notificationMap: MapType<(name: State) => NotificationConstructorOptions> = {
+    [Status.FAILURE]: (state: State) => ({
+      title: 'Fail',
+      body: `${state.name} failed`,
+      icon: nativeImage.createFromPath(join(__dirname, '..', 'assets', 'fail_icon_big.png')),
+    }),
+    [Status.SUCCESS]: (state: State) => ({
+      title: 'Success',
+      body: `${state.name} Success`,
+      icon: nativeImage.createFromPath(join(__dirname, '..', 'assets', 'ok_icon_big.png')),
+    }),
+    [Status.CHECKING]: (state: State) => ({
+      title: 'Checking',
+      body: `${state.name} Checking`,
+      icon: nativeImage.createFromPath(join(__dirname, '..', 'assets', 'running_icon_big.png')),
+    }),
+    [Status.NA]: (state: State) => ({
+      title: 'Unaccesible',
+      body: `${state.name} Unaccesible`,
+      icon: nativeImage.createFromPath(join(__dirname, '..', 'assets', 'na_icon_big.png')),
+    }),
+  };
 
   public updateNotifications(old: State[], actual: State[]) {
     actual.forEach((current) => {
       const previous = old.find(({ name }) => name === current.name) || ({ status: Status.NA } as any);
-      if (previous.status === Status.SUCCESS && current.status === Status.FAILURE) {
-        return new Notification({
-          title: 'Fail',
-          body: `${current.name} failed`,
-          icon: nativeImage.createFromPath(this.FailIconPath),
-        }).show();
-      }
-      if (previous.status === Status.FAILURE && current.status === Status.SUCCESS) {
-        return new Notification({
-          title: 'Fix',
-          body: `${current.name} fixed`,
-          icon: nativeImage.createFromPath(this.okIconPath),
-        }).show();
-      }
+      if (current.status === previous.status) return;
+      return new Notification(this.notificationMap[current.status](current));
     });
   }
 }
