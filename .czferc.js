2f85ca1940bfe1d909ce8597783b293bd6a69ce6
kanekotic <me@kanekotic.com>
2022-08-03 16:34:07 +0200

    chore(tools): improved questions
    kanekotic2 <me@kanekotic.com>

--- .czferc.js
+++ .czferc.js
@@ -1,13 +1,60 @@
-// ==============================
-// Base on https://github.com/commitizen/cz-conventional-changelog
-// ==============================
-
-const { types } = require('./types.json')
-
+const { contributors } = require('./package.json')
+//Co-authored-by: name <name@example.com>
   /**
    * @typedef {{type: string; scope: string; subject: string; body: string; isBreaking: boolean; breakingBody: string; breaking: string; isIssueAffected: boolean; issuesBody: string; issues: string;}} Answers
    */
-
+const types = [
+    {
+      "name": "feat: A new feature",
+      "value": "feat"
+    },
+    {
+      "name": "fix: A bug fix",
+      "value": "fix"
+    },
+    {
+      "name": "docs: Documentation only changes",
+      "value": "docs"
+    },
+    {
+      "name": "style: Changes that do not affect the meaning of the code (white-space, formatting, missing semi-colons, etc)",
+      "value": "style"
+    },
+    {
+      "name": "refactor: A code change that neither fixes a bug nor adds a feature",
+      "value": "refactor"
+    },
+    {
+      "name": "perf: A code change that improves performance",
+      "value": "perf"
+    },
+    {
+      "name": "test: Adding missing tests or correcting existing tests",
+      "value": "test"
+    },
+    {
+      "name": "chore: Other changes that don't modify src or test files",
+      "value": "chore"
+    },
+    {
+      "name": "revert: Reverts a previous commit",
+      "value": "revert"
+    }
+  ]
+const scopes = [
+    {
+      "name": "core: base system of the application",
+      "value": "core"
+    },
+    {
+      "name": "extensions: systems that are observed",
+      "value": "extensions"
+    },
+    {
+      "name": "tools: other things in the project",
+      "value": "tools"
+    },
+]
 /** @type import('cz-format-extension').Config<Answers> */
 module.exports = {
   questions({inquirer}) {
@@ -15,14 +62,14 @@ module.exports = {
       {
         type: "list",
         name: "type",
-        message: "Select type",
+        message: "'What is the type of this change:",
         choices: types
       },
       {
-        type: 'input',
+        type: 'list',
         name: 'scope',
-        message:
-          'What is the scope of this change (e.g. component or file name): (press enter to skip)\n'
+        message: 'What is the scope of this change:',
+        choices: scopes
       },
       {
         type: 'input',
@@ -43,14 +90,6 @@ module.exports = {
       },
       {
         type: 'input',
-        name: 'breakingBody',
-        default: '-',
-        message:
-          'A BREAKING CHANGE commit requires a body. Please enter a longer description of the commit itself:\n',
-        when: answers => answers.isBreaking && !answers.body
-      },
-      {
-        type: 'input',
         name: 'breaking',
         message: 'Describe the breaking changes:\n',
         when: answers => answers.isBreaking
@@ -63,22 +102,31 @@ module.exports = {
       },
       {
         type: 'input',
-        name: 'issuesBody',
-        default: '-',
-        message:
-          'If issues are closed, the commit requires a body. Please enter a longer description of the commit itself:\n',
-        when: answers => answers.isIssueAffected && !answers.body && !answers.breakingBody
-      },
-      {
-        type: 'input',
         name: 'issues',
-        message: 'Add issue references (e.g. "fix #123", "re #123".):\n',
+        message: 'Add issue references:\n',
         when: answers => answers.isIssueAffected,
         default: undefined,
         validate: (issues) => issues.length === 0 ? 'issues is required' : true
-      }
+      },
+      {
+        type: 'checkbox',
+        name: 'coauthors',
+        message: 'Select Co-Authors if any:',
+        choices: contributors.map(contributor => ({
+            name: contributor.name,
+            value: `${contributor.name} <${contributor.email}>`,
+        }))
+      },
+      {
+        type: 'checkbox',
+        name: 'coauthors',
+        message: 'Select Co-Authors if any:',
+        choices: contributors.map(contributor => ({
+            name: contributor.name,
+            value: `Co-authored-by: ${contributor.name} <${contributor.email}>`,
+        }))
+      },
     ]
-
   },
   commitMessage({answers}) {
     const scope = answers.scope ? `(${answers.scope})` : '';
@@ -86,7 +134,8 @@ module.exports = {
     const body = answers.body ? answers.body : '';
     const breaking = answers.breaking ? `BREAKING CHANGE: ${answers.breaking}` : '';
     const issues = answers.issues ? answers.issues : '';
+    const coauthors = answers.coauthors.join('\n');
 
-    return [head, body, breaking, issues].join('\n\n').trim()
+    return [head, body, breaking, issues, coauthors].join('\n\n').trim()
   }
 }
